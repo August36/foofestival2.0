ea0b5f4b7c0c6a04babb911296ad2029ed1834d1
August <augustjp@live.dk>
2024-06-13 18:11:33 +0200

    removed inconsistent style

--- src/app/receipt/page.jsx
+++ src/app/receipt/page.jsx
@@ -4,7 +4,7 @@ import Receipt from "../components/Receipt";
 
 const page = () => {
   return (
-    <section className="mt-16 mb-8 rounded-lg shadow-md lg:max-w-screen-lg xl:max-w-screen-lg 2xl:max-w-screen-lg lg:mx-auto xl:mx-auto 2xl:mx-auto md:mx-0">
+    <section className="mt-16 mb-8 lg:max-w-screen-lg xl:max-w-screen-lg 2xl:max-w-screen-lg lg:mx-auto xl:mx-auto 2xl:mx-auto md:mx-0">
       <Receipt />
     </section>
   );
